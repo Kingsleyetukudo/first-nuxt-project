b17708b992e69eb783c0db46d5e8a8ab6000f250
Kingsley Etukudo <kingsleyj90@gmail.com>
2023-01-21 10:10:44 +0100

    first nuxt project build

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, cumque magni
+      facilis repellendus ratione dolorem nesciunt? Atque error non totam!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, cumque magni
+      facilis repellendus ratione dolorem nesciunt? Atque error non totam!
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+
+p {
+  margin: 20px 0;
+}
+</style>
