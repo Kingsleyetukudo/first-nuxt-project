4d45768b591a5c127c35e271b09a33cd5d0d2f03
Kingsley Etukudo <kingsleyj90@gmail.com>
2023-01-21 10:36:36 +0100

    added route to a nuxt page

---
+++ pages/products/[id].vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>product id for {{ id }}</p>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus sint
+      sequi ducimus animi beatae distinctio quisquam. Rerum autem fugit
+      reprehenderit.
+    </p>
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params;
+</script>
+
+<style scoped></style>
